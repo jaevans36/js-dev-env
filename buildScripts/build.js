e66be5cca8192366adcb88e14edb83046d3b33db
Jay <jaevans36@gmail.com>
2017-11-06 20:24:25 +0000

    Added changes for vendor hashing and fixed HtmlWebpackPlugin

--- buildScripts/build.js
+++ buildScripts/build.js
@@ -1,15 +1,16 @@
-/* eslint-disable no-console */
+// More info on Webpack's Node API here: https://webpack.github.io/docs/node.js-api.html
+// Allowing console calls below since this is a build file.
+/*eslint-disable no-console */
 import webpack from 'webpack';
 import webpackConfig from '../webpack.config.prod';
 import chalk from 'chalk';
 
-// Used by Babel to determine how the production environment is built
-process.env.NODE_ENV = 'production';
+process.env.NODE_ENV = 'production'; // this assures the Babel dev config doesn't apply.
 
-console.log(chalk.blue('Generated minified bundle for production. This will take a moment...'));
+console.log(chalk.blue('Generating minified bundle for production. This will take a moment...'));
 
 webpack(webpackConfig).run((err, stats) => {
-  if (err) { // so a fatal error occurred, Stop here.
+  if (err) { // so a fatal error occurred. Stop here.
     console.log(chalk.red(err));
     return 1;
   }
@@ -21,13 +22,13 @@ webpack(webpackConfig).run((err, stats) => {
   }
 
   if (jsonStats.hasWarnings) {
-    console.log(chalk.yellow('Webpack generated the following warning: '));
+    console.log(chalk.yellow('Webpack generated the following warnings: '));
     jsonStats.warnings.map(warning => console.log(chalk.yellow(warning)));
   }
 
   console.log(`Webpack stats: ${stats}`);
 
-  // If we got this far, the build was successful
+  // if we got this far, the build succeeded.
   console.log(chalk.green('Your app has been built for production and written to /dist!'));
 
   return 0;
